89a9d9827df8a451dd8376ca3273bdf6feb16610
payerne <constantin.payerne@etu.univ-grenoble-alpes.fr>
2025-07-14 18:39:16 +0200

    new changes

--- modules/cluster/cluster_catalogue.py
+++ modules/cluster/cluster_catalogue.py
@@ -16,7 +16,7 @@ class ClusterCatalogue:
     def get_cluster_catalogue(self, log10m_true, z_true):
 
         
-        if self.settings['cluster_catalogue']['completeness']=='True':
+        if self.settings['cluster_catalogue']['add_completeness']=='True':
             a=1
             u = np.random.random(len(log10m_true))
             mask = u < _completeness.completeness(log10m_true, z_true)
@@ -38,11 +38,15 @@ class ClusterCatalogue:
                                                                  rho, which = which)
 
         richness, log10mWL, z_true = MoR.generate_mWL_richness(log10m_true, z_true)
-        z_obs = _photometric_redshift.photometric_redshift(z_true, float(self.settings['cluster_catalogue.photometric_redshift']['sigma_z0']))
-        z_obs[z_obs < 0] = None
+        
+        if self.settings['cluster_catalogue']['add_photometric_redshift']=='True': 
+            sigma_z0 = float(self.settings['cluster_catalogue.photometric_redshift']['sigma_z0'])
+            z_obs = _photometric_redshift.photometric_redshift(z_true, sigma_z0)
+            z_obs[z_obs < 0] = None
+        else: 
+            z_obs = z_true
 
-        if self.settings['cluster_catalogue']['purity']=='True': 
-            
+        if self.settings['cluster_catalogue']['add_purity']=='True': 
             richness_edges = np.logspace(np.log10(20), np.log10(300), 70) 
             richness_center = 0.5 * (richness_edges[:-1] + richness_edges[1:])
             z_obs_edges = np.linspace(0, 2, 70)
@@ -71,9 +75,11 @@ class ClusterCatalogue:
             richness = np.array(list(richness) + fake_richness)
             log10mWL = np.array(list(log10mWL) + fake_log10mWL)
             z_obs = np.array(list(z_obs) + fake_z_obs)
+        
+        if self.settings['cluster_catalogue']['add_selection']=='True': 
+            mask_selection = _selection.selection(richness, z_obs)
+            richness, log10mWL, z_obs = richness[mask_selection], log10mWL[mask_selection], z_obs[mask_selection]
 
-        mask_selection = _selection.selection(richness, z_obs)
-
-        return richness[mask_selection], log10mWL[mask_selection], z_obs[mask_selection]
+        return richness, log10mWL, z_obs
 
 
