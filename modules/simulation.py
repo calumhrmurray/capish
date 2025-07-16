4cb6de1f2bfc0e98452a401b25439e8fcfe08773
payerne <constantin.payerne@etu.univ-grenoble-alpes.fr>
2025-07-16 10:22:28 +0200

    run_simulation works

--- modules/simulation.py
+++ modules/simulation.py
@@ -43,50 +43,50 @@ class UniverseSimulator:
         return config_new
 
 
-    def run_simulation( self , variable_param_values):
+    def run_simulation(self, variable_params_values):
         """
         Run the simulation using the variable parameters provided.
         """
 
-        config_new = new_config_files(variable_params_values)
+        config_new = self.new_config_files(variable_params_values)
         log10m_true, z_true = self.halo_catalogue_class.get_halo_catalogue( config_new )
         richness, log10mWL, z_obs = self.cluster_catalogue_class.get_cluster_catalogue( log10m_true, z_true , config_new )
-        summary_statistic = self.get_summary_statistic( richness, log10mWL, z_obs, config_new )
+        summary_statistic = self.summary_statistics_class.get_summary_statistics( richness, log10mWL, z_obs, config_new )
 
         return summary_statistic
 
-    def _get_parameter_set(self, param_values):
-        """
-        Create the full parameter set by combining fixed and variable parameters.
-        """
-        # Start with default parameter values
-        parameter_set = self.available_params.copy()
+#     #def _get_parameter_set(self, param_values):
+#      #   """
+#      #   Create the full parameter set by combining fixed and variable parameters.
+#      #   """
+#      #   # Start with default parameter values
+#       #  parameter_set = self.available_params.copy()
+# #
+#         # Assign the passed variable parameters to their corresponding keys
+#       #  for i, param in enumerate( self.variable_params ):
+#       #      parameter_set[param] = float(param_values[i])  # Ensure float type
 
-        # Assign the passed variable parameters to their corresponding keys
-        for i, param in enumerate( self.variable_params ):
-            parameter_set[param] = float(param_values[i])  # Ensure float type
+#         # Overwrite with any fixed parameters
+#       #  parameter_set.update( self.fixed_params)
+# #
+#         # Return a dictionary of the parameters
+#       #  return parameter_set
 
-        # Overwrite with any fixed parameters
-        parameter_set.update( self.fixed_params)
+#   #  def run_simulation(self, param_values):
+#    #     """
+#    #@     Run the simulation using the variable parameters provided.
+#    #     """
+#    #     # Get the full parameter set (both variable and fixed parameters)
+#    #     parameter_set = self._get_parameter_set( param_values )
 
-        # Return a dictionary of the parameters
-        return parameter_set
+#         # Run the core simulation
+#         richness, log10M_wl, z_clusters, _  = self._run_simulation( parameter_set )
 
-    def run_simulation(self, param_values):
-        """
-        Run the simulation using the variable parameters provided.
-        """
-        # Get the full parameter set (both variable and fixed parameters)
-        parameter_set = self._get_parameter_set( param_values )
-
-        # Run the core simulation
-        richness, log10M_wl, z_clusters, _  = self._run_simulation( parameter_set )
-
-        # Return result in a format compatible with SBI
-        if self.for_simulate_for_sbi:
-            return self.summary_statistic(richness, log10M_wl, z_clusters)
-        #     return torch.tensor( self.summary_statistic(richness, log10M_wl, z_clusters))
-        else:
-            return self.summary_statistic(richness, log10M_wl, z_clusters)
+#         # Return result in a format compatible with SBI
+#         if self.for_simulate_for_sbi:
+#             return self.summary_statistic(richness, log10M_wl, z_clusters)
+#         #     return torch.tensor( self.summary_statistic(richness, log10M_wl, z_clusters))
+#         else:
+#             return self.summary_statistic(richness, log10M_wl, z_clusters)
 
     
