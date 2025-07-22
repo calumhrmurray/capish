afb3afc2581dca75c97979e69b58312d3bc7e830
payerne <constantin.payerne@etu.univ-grenoble-alpes.fr>
2025-07-22 15:13:24 +0200

    small changes

--- modules/cluster/_completeness.py
+++ modules/cluster/_completeness.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def completeness(log10m, z):
+def completeness(log10m, z, params = None):
     r"""
     Attributes:
     -----------
@@ -15,9 +15,9 @@ def completeness(log10m, z):
     completeness : array
         completeness of cluster detection
     """
-    a_nc, b_nc, a_mc, b_mc = 1.1321, 0.7751, 13.31, 0.2025
-    theta_completeness = [a_nc, b_nc, a_mc, b_mc]
-    a_nc, b_nc, a_mc, b_mc = theta_completeness
+    if params==None:
+        a_nc, b_nc, a_mc, b_mc = 1.1321, 0.7751, 13.31, 0.2025
+    else: a_nc, b_nc, a_mc, b_mc = params
     nc = a_nc + b_nc*(1+z)
     log10mc = a_mc + b_mc*(1+z)
     return np.exp(nc*np.log(10)*(log10m-log10mc))/(1+np.exp(nc*np.log(10)*(log10m-log10mc)))
--- modules/cluster/_purity.py
+++ modules/cluster/_purity.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def purity(richness, z):
+def purity(richness, z, params = None):
     r"""
     Attributes:
     -----------
@@ -15,9 +15,11 @@ def purity(richness, z):
     purity : array
         purity of cluster detection
     """
-    a_nc, b_nc, a_rc, b_rc = np.log(10)*0.8612, np.log(10)*0.3527, 2.2183, -0.6592
-    theta_purity = [a_nc, b_nc, a_rc, b_rc]
-    a_nc, b_nc, a_rc, b_rc = theta_purity
+    if params==None:
+        a_nc, b_nc, a_rc, b_rc = np.log(10)*0.8612, np.log(10)*0.3527, 2.2183, -0.6592
+        theta_purity = [a_nc, b_nc, a_rc, b_rc]
+    else: 
+        a_nc, b_nc, a_rc, b_rc = params
     nc = a_nc + b_nc * (1 + z)
     lnrc = a_rc + b_rc * (1 + z)
     lnr = np.log(richness)
