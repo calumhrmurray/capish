76cada9bd09dcdc2f46abc14c9a3f7e345a1be68
calumhrmurray <calumhrmurray@gmail.com>
2025-09-09 10:21:11 +0200

    I removed the if statements in the __init__ of SummaryStatistics so that this can be generic.

--- modules/summary_statistics/summary_statistics.py
+++ modules/summary_statistics/summary_statistics.py
@@ -45,54 +45,8 @@ class SummaryStatistics:
     def __init__( self , default_config):
         
         self.default_config = default_config
-
-        if str(default_config['summary_statistics']['richness_edges']) == 'None':
-            log10lambda_low = np.log10(float(default_config['summary_statistics']['richness_lower_limit']))
-            log10lambda_up = np.log10(float(default_config['summary_statistics']['richness_upper_limit']))
-            n_bins_lambda = int(default_config['summary_statistics']['n_bins_richness'])
-            self.richness_edges = np.logspace(log10lambda_low, log10lambda_up, n_bins_lambda+1)
-            self.richness_centers = 0.5 * (self.richness_edges[:-1] + self.richness_edges[1:])
-        else: 
-            self.richness_edges = np.array([float(k) for k in default_config['summary_statistics']['richness_edges'].split(', ')])
-            self.richness_centers = 0.5 * (self.richness_edges[:-1] + self.richness_edges[1:])
         
-        if str(default_config['summary_statistics']['redshift_edges']) == 'None':
-            redshift_low = float(default_config['summary_statistics']['redshift_lower_limit'])
-            redshift_up = float(default_config['summary_statistics']['redshift_upper_limit'])
-            n_bins_redshift = int(default_config['summary_statistics']['n_bins_redshift'])
-            self.redshift_edges = np.linspace(redshift_low, redshift_up, n_bins_redshift+1)
-            self.redshift_centers = 0.5 * (self.redshift_edges[:-1] + self.redshift_edges[1:])
-        else: 
-            self.redshift_edges = np.array([float(k) for k in default_config['summary_statistics']['redshift_edges'].split(', ')])
-            self.redshift_centers = 0.5 * (self.redshift_edges[:-1] + self.redshift_edges[1:])
-
-        if str(default_config['summary_statistics']['log10mWL_edges']) == 'None':
-            log10mWL_low = float(default_config['summary_statistics']['log10mWL_lower_limit'])
-            log10mWL_up = float(default_config['summary_statistics']['log10mWL_upper_limit'])
-            n_bins_log10mWL = int(default_config['summary_statistics']['n_bins_log10mWL'])
-            self.log10mWL_edges = np.linspace(log10mWL_low, log10mWL_up, n_bins_log10mWL+1)
-            self.log10mWL_centers = 0.5 * (self.log10mWL_edges[:-1] + self.log10mWL_edges[1:])
-        else: 
-            self.log10mWL_edges = np.array([float(k) for k in default_config['summary_statistics']['log10mWL_edges'].split(', ')])
-            self.log10mWL_centers = 0.5 * (self.log10mWL_edges[:-1] + self.log10mWL_edges[1:])
-
-        Omega_c_fid = float(default_config['halo_catalogue']['Omega_c_fiducial'])
-        Omega_b_fid = float(default_config['halo_catalogue']['Omega_b_fiducial'])
-        sigma8_fid = float(default_config['halo_catalogue']['sigma_8_fiducial'])
-        h_fid = float(default_config['halo_catalogue']['h_fiducial'])
-        ns_fid = float(default_config['halo_catalogue']['n_s_fiducial'])
-
-        self.Gamma = float(default_config['summary_statistics']['Gamma'])
-        
-        cosmo_fid = ccl.Cosmology( Omega_c = Omega_c_fid, Omega_b = Omega_b_fid, 
-                                  h = h_fid, sigma8 = sigma8_fid, n_s= ns_fid)
-        
-        z_l_array = np.linspace(0.03, 3, 300)
-        W_zl = lensing_weights(cosmo_fid, z_l_array, z_s_max=3.0, n_zs=500, sigma_e_const=0.3)
-        def W_zl_f(z_l): return np.interp(z_l, z_l_array, W_zl)
-        self.W_zl_f = W_zl_f
-        
-        return None
+        return 
 
     def get_summary_statistics(self, richness, log10mWL, z_obs, config_new):
 
