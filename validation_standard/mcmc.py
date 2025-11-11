806a7d4d6e69b3396aea5376a6ec34bdc82d10f8
payerne <constantin.payerne@etu.univ-grenoble-alpes.fr>
2025-11-11 16:17:32 +0100

    add theory snr

--- validation_standard/mcmc.py
+++ validation_standard/mcmc.py
@@ -83,22 +83,14 @@ params_fid = 0.298, 0.8, -9.348, 0.75, 0.3
 ClusterAbundanceObject_fid, N_fid, b_fid, log10M_fid = compute_count_mass(params_fid, count=True, mass=True, recompute_bias=True)
 params_default = ClusterAbundanceObject_fid.params_default 
 cosmo_fid = params_default['CCL_cosmology']
-log10M_fid_err = ClusterAbundanceObject_fid.model_error_log10m_one_cluster(10**log10M_fid, cosmo_fid, 
+log10M_th_err = ClusterAbundanceObject_fid.model_error_log10m_one_cluster(10**log10M_fid, cosmo_fid, 
                                                                            Rmin=1, Rmax=5, 
                                                                            ngal_arcmin2=25, shape_noise=0.25, 
                                                                            delta=200, mass_def='mean',
                                                                            sigma_A_prior = 0.03,
                                                                            sigma_c_prior=0.1,
                                                                            cM ='Duffy08')
-log10M_fid_err *= 1/np.sqrt(N_fid)
-print('==mass==')
-print('fid logMass= ',log10M_fid[:,0])
-print('data logMass=',log10M_data[:,0])
-print('fid logmass_err=', log10M_fid_err[:,0])
-print()
-print('==count==')
-print('fid count= ', N_fid[:,0])
-print('data count=', N_data[:,0])
+log10M_th_err *= 1/np.sqrt(N_fid)
 
 skyarea_fid = ClusterAbundanceObject_fid.HaloAbundanceObject.sky_area
 fsky_fid = skyarea_fid/(4*np.pi)
@@ -120,7 +112,21 @@ if analysis['SSC_count_covariance']:
         return cov_SSC
     cov_SSC_fid = SSC_cov(N_fid * b_fid, Sij)
 
-log10M_th_err = log10M_fid_err
+if analysis['use_fiducial_data_vector'] == False:
+    log10M_data = log10M_data
+    N_data = N_data
+else:
+    log10M_data = log10M_fid
+    N_data = N_fid
+    
+print('==mass==')
+print('fid logMass= ',log10M_fid[:,0])
+print('data logMass=',log10M_data[:,0])
+print('fid logmass_err=', log10M_th_err[:,0])
+print()
+print('==count==')
+print('fid count= ', N_fid[:,0])
+print('data count=', N_data[:,0])
 
 def likelihood(params):
 
@@ -145,21 +151,17 @@ def likelihood(params):
 
     if analysis['summary_stat'] == 'count_only' or analysis['summary_stat'] == 'count_mass':
 
-        if analysis['SSC_count_covariance']==False: 
-            Covariance_count = np.diag( N_th.flatten() )
+        Covariance_count = np.diag( N_th.flatten() )
             
         if analysis['SSC_count_covariance']: 
-            Covariance_count = np.diag( N_th.flatten() )
             Covariance_count += cov_SSC_fid
 
         diff_N = N_data.flatten() - N_th.flatten()
         invC = np.linalg.inv(Covariance_count)
-        term1 = np.sum(diff_N.T @ invC @ diff_N)
-        sign, logdet = np.linalg.slogdet(Covariance_count)
-        ln_likelihood_count = - 0.5 * (term1)# + logdet)
+        ln_likelihood_count = - 0.5 * np.sum(diff_N.T @ invC @ diff_N)
 
     if analysis['summary_stat'] == 'mass_only' or analysis['summary_stat'] == 'count_mass':
-        ln_likelihood_mass = - 0.5*np.sum( ((log10M_data - log10M_th)/log10M_th_err )**2)
+        ln_likelihood_mass = - 0.5 * np.sum( ((log10M_data - log10M_th)/log10M_th_err )**2)
 
     if analysis['summary_stat'] == 'count_only': 
         return ln_likelihood_count
@@ -185,11 +187,11 @@ print(f'[First test]: lnL(initial) = {lnL_start:.2f} computed in {tf-t:.2f} seco
 #from scipy.optimize import minimize
 
 nwalker = 150
-nstep = 200
+nstep = 300
 ndim = len(initial)
 print('[MCMC]: time of the MCMC:', nwalker * nstep * (tf-t)/3600, ' hours')
 pos = np.array(initial) + .01*np.random.randn(nwalker, ndim)
-pos = pos[(pos[:,0] > 0)*(pos[:,0] < 1)*(pos[:,1] > 0.5)*(pos[:,1] < 1)*(pos[:,4] > 0)]
+pos = pos[(pos[:,0] > 0.1)*(pos[:,0] < 1)*(pos[:,1] > 0.5)*(pos[:,1] < 1)*(pos[:,4] > 0)]
 nwalker = len(pos[:,0])
 filename = analysis['name_save'] + '.h5'
 backend = emcee.backends.HDFBackend(filename)
